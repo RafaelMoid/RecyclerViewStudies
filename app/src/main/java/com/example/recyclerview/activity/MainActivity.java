72b3cf17ff2f1ed8632bc1c1e73992559f24f618
Rafael Varela <rafaelvrcr@gmail.com>
2021-06-08 16:36:27 -0300

    Creating a infinity scroll list that recycles all objects from a list. until now we've created New classes, subdvide the project in 3 folders (Activity, Model ...)

---
+++ app/src/main/java/com/example/recyclerview/activity/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.recyclerview.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.recyclerview.R;
+import com.example.recyclerview.adapter.Adapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //Configurar adapter
+        Adapter adapter = new Adapter();
+
+
+        //Configurar RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager( getApplicationContext() );
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+
+    }
+}
